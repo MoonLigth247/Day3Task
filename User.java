af8a2b501f128d8ce8c5f1dace3b66e3618826d8
Tugu8 <tugutugu018@gmail.com>
2025-06-06 12:23:21 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,12 +1,36 @@
-class User {
+/**
+ * Системийн хэрэглэгчийн мэдээллийг хадгалах класс.
+ */
+public class User {
     private String email;
     private String name;
     private String password;
 
+    /**
+     * Хэрэглэгчийн шинэ обьектыг үүсгэнэ.
+     *
+     * @param email хэрэглэгчийн и-мэйл хаяг
+     * @param name хэрэглэгчийн бүтэн нэр
+     * @param password хэрэглэгчийн нууц үг
+     */
     public User(String email, String name, String password) {
         this.email = email;
         this.name = name;
         this.password = password;
     }
-}
 
+    /**
+     * @return хэрэглэгчийн и-мэйл хаяг
+     */
+    public String getEmail() { return email; }
+
+    /**
+     * @return хэрэглэгчийн бүтэн нэр
+     */
+    public String getName() { return name; }
+
+    /**
+     * @return хэрэглэгчийн нууц үг
+     */
+    public String getPassword() { return password; }
+}
