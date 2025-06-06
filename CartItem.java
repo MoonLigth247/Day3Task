90d8dfdb8b254298920f1398cfe71bac803b3b28
Tugu8 <tugutugu018@gmail.com>
2025-06-06 12:22:01 +0800

    Update CartItem.java

--- CartItem.java
+++ CartItem.java
@@ -1,13 +1,35 @@
-class CartItem {
+/**
+ * Сагсанд нэмсэн бүтээгдэхүүний мэдээллийг хадгалах класс.
+ */
+public class CartItem {
     private Product product;
     private int quantity;
 
+    /**
+     * Сагсны шинэ обьектыг үүсгэнэ.
+     *
+     * @param product сагсанд нэмэх бүтээгдэхүүн
+     * @param quantity бүтээгдэхүүний тоо ширхэг
+     */
     public CartItem(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     }
 
+    /**
+     * @return сагсанд байгаа бүтээгдэхүүн
+     */
     public Product getProduct() { return product; }
+
+    /**
+     * @return тухайн бүтээгдэхүүний тоо ширхэг
+     */
     public int getQuantity() { return quantity; }
+
+    /**
+     * Тухайн бүтээгдэхүүний нийт үнийг тооцоолно (үнэ × тоо ширхэг).
+     *
+     * @return бүтээгдэхүүний нийт үнэ
+     */
     public double getTotal() { return product.getPrice() * quantity; }
 }
